395798a9c4d58918f85602ca6b9701379be6bd65
felix <felixxplore08@gmail.com>
2025-05-27 13:14:36 +0530

    fix db connection issue

--- src/main/java/com/felix/QuizApp/QuizAppApplication.java
+++ src/main/java/com/felix/QuizApp/QuizAppApplication.java
@@ -1,5 +1,6 @@
 package com.felix.QuizApp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuizAppApplication {
 
 	public static void main(String[] args) {
+
+		// Load .env variables into System properties
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(QuizAppApplication.class, args);
 	}
 
