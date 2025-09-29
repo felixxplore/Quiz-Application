b4fd83d027d6e3308fb1542af7423e8a9cb87e9e
felix <felixxplore08@gmail.com>
2025-09-29 18:37:43 +0530

    add sandgrind email service

--- src/main/java/com/felix/QuizApp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/felix/QuizApp/exceptions/GlobalExceptionHandler.java
@@ -12,26 +12,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password "+ ex.getMessage());
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found " + ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong" + ex.getMessage());
     }
     @ExceptionHandler(EmailNotVerifiedException.class)
     public ResponseEntity<String> handleEmailNotVerifiedException(EmailNotVerifiedException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("email is not varified "+ex.getMessage());
     }
 
     @ExceptionHandler(InvalidJwtAuthenticationException.class)
     public ResponseEntity<String> invalidateTokenException(InvalidJwtAuthenticationException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid JWT token to access " + ex.getMessage());
     }
 }
 
--- src/main/java/com/felix/QuizApp/service/AuthService.java
+++ src/main/java/com/felix/QuizApp/service/AuthService.java
@@ -76,11 +76,11 @@ public class AuthService {
         userRepository.save(user);
 
         // Generate Verification Token
-//        String token=verificationTokenService.generateVerificationToken(user);
+        String token=verificationTokenService.generateVerificationToken(user);
 
-//        sendVerificationEmail(user.getEmail(),token);
+       emailService.sendVerificationEmail(user.getEmail(),token);
 
-//        System.out.println("Verification link: http://localhost:8080/api/auth/verify-email?token="+token);
+        System.out.println("Verification link: "+baseUrl +"/api/auth/verify-email?token="+token);
     }
 
     /**
